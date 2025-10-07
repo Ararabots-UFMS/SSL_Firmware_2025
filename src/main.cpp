967dd6ff5c83a66473e139f2713e00d75528d2a5
Fabio Huang <fabio_huang@ufms.br>
2025-10-07 16:19:12 -0400

    esse funciona

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,14 @@ BLDCDriver3PWM driver2 = BLDCDriver3PWM(PA8, PA9, PA10, PA11);
 BLDCDriver3PWM driver3 = BLDCDriver3PWM(PB6, PB7, PB8, PB9);
 BLDCDriver3PWM driver4 = BLDCDriver3PWM(PC6, PC7, PC8, PC9);
 
-// SPIClass sensor_spi1(PB5, PB4, PB3);
-// MagneticSensorSPI encoder = MagneticSensorSPI(PD6, 14, 0x3FFF);
+SPIClass sensor_spi1(PB5, PB4, PB3);
+MagneticSensorSPI encoder2 = MagneticSensorSPI(AS5047_SPI, PD4);
+MagneticSensorSPI encoder3 = MagneticSensorSPI(AS5047_SPI, PD6);
+
+// SPIClass sensor_spi2(PA7, PA6, PA5);
+MagneticSensorSPI encoder1 = MagneticSensorSPI(AS5047_SPI, PA2);
+MagneticSensorSPI encoder4 = MagneticSensorSPI(AS5047_SPI, PA4);
+
 
 #define ROBOT_RADIUS 0.072f  // Robot radius in meters
 #define WHEEL_RADIUS 0.034f  // Wheel radius in meters
@@ -43,10 +49,19 @@ const float WHEEL_ANGLES[4] = {
 
 float jacobian[4][3];
 
-
 void setup() {    
     pinMode(PE3, OUTPUT);
-    Serial.begin(9600);
+    Serial.begin(115200);
+    
+    encoder1.init(&sensor_spi1);
+    encoder2.init(&sensor_spi1);
+    encoder3.init(&sensor_spi1);
+    encoder4.init(&sensor_spi1);
+
+    motor1.linkSensor(&encoder1);
+    motor2.linkSensor(&encoder2);
+    motor3.linkSensor(&encoder3);
+    motor4.linkSensor(&encoder4);
     
     for (int i = 0; i < 4; i++) {
         jacobian[i][0] = sin(WHEEL_ANGLES[i]);
@@ -65,12 +80,6 @@ void setup() {
     driver2.voltage_limit = 12;
     driver3.voltage_limit = 12;
     driver4.voltage_limit = 12;
-    
-//     digitalWrite(PE3, HIGH);
-//     delay(1000);
-//         digitalWrite(PE3, LOW);
-//     encoder.init();
-//     motor2.linkSensor(&encoder);
 
     driver1.init();
     driver2.init();
@@ -82,12 +91,30 @@ void setup() {
     motor3.linkDriver(&driver3);
     motor4.linkDriver(&driver4);
     
-//     motor2.controller = MotionControlType::velocity;
-    
-    motor1.controller = MotionControlType::velocity_openloop;
-    motor2.controller = MotionControlType::velocity_openloop;
-    motor3.controller = MotionControlType::velocity_openloop;
-    motor4.controller = MotionControlType::velocity_openloop;
+    motor1.controller = MotionControlType::velocity;
+    motor2.controller = MotionControlType::velocity;
+    motor3.controller = MotionControlType::velocity;
+    motor4.controller = MotionControlType::velocity;
+
+    motor1.PID_velocity.P = 0.2f;
+    motor1.PID_velocity.I = 20;
+    motor1.PID_velocity.D = 0;
+    motor1.PID_velocity.output_ramp = 1000;
+
+    motor2.PID_velocity.P = 0.2f;
+    motor2.PID_velocity.I = 20;
+    motor2.PID_velocity.D = 0;
+    motor2.PID_velocity.output_ramp = 1000;
+
+    motor3.PID_velocity.P = 0.2f;
+    motor3.PID_velocity.I = 20;
+    motor3.PID_velocity.D = 0;
+    motor3.PID_velocity.output_ramp = 1000;
+
+    motor4.PID_velocity.P = 0.2f;
+    motor4.PID_velocity.I = 20;
+    motor4.PID_velocity.D = 0;
+    motor4.PID_velocity.output_ramp = 1000;
     
     motor1.init();
     motor2.init();
@@ -115,6 +142,7 @@ void calculateWheelSpeeds(float vx, float vy, float vt, float* result) {
 
 static uint8_t msg_count = 0;
 static uint8_t led_state = LOW;
+float target_velocity = 20.0;
 
 void loop() {
     RobotCommand cmd;
